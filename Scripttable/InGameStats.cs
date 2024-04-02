5bdaa34781de97eb193c534ca2af898d4809474c
BAYU ARDIYANSYAH <54973139+RazerArdi@users.noreply.github.com>
2024-04-02 16:52:48 +0700

    Add files via upload

---
+++ Scripttable/InGameStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "In-Game Stats")]
+public class InGameStats : ScriptableObject
+{
+    public float awaySoccerMovement;
+    public float awayTennisMovement;
+
+}
